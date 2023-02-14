d54781f17dcf094880d96dac1e59ea7f3c8e6e09
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-02-14 21:37:39 +0100

    (11) eigth gold star

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -17,26 +17,33 @@ public class Day4 {
 
             List<String> firstPair = List.of((onePair.get(0).split("-")));
             List<String> secondPair = List.of((onePair.get(1).split("-")));
-
+            //1
             int firstBoundaryFirstPair = Integer.parseInt(firstPair.get(0));
+            //2
             int secondBoundaryFirstPair = Integer.parseInt(firstPair.get(1));
-
+            //3
             int firstBoundarySecondPair = Integer.parseInt(secondPair.get(0));
+            //4
             int secondBoundarySecondPair = Integer.parseInt(secondPair.get(1));
 
             //this checks if the first pair is in the second pair included
-            if((firstBoundaryFirstPair >= firstBoundarySecondPair && firstBoundaryFirstPair <= secondBoundarySecondPair) &&
-                (secondBoundaryFirstPair >= firstBoundarySecondPair && secondBoundaryFirstPair <= secondBoundarySecondPair)){
-                    count += 1;
-                    continue;
-                }
-                
-            //this checks if the second pair is in the first pair included
-            if((firstBoundaryFirstPair <= firstBoundarySecondPair && firstBoundarySecondPair <= secondBoundaryFirstPair) &&
-                (firstBoundaryFirstPair <= secondBoundarySecondPair && secondBoundaryFirstPair >= secondBoundarySecondPair)){
-                    count += 1;
-                    continue;
-                }
+
+            //if((firstBoundaryFirstPair >= firstBoundarySecondPair && firstBoundaryFirstPair <= secondBoundarySecondPair) &&
+            //    (secondBoundaryFirstPair >= firstBoundarySecondPair && secondBoundaryFirstPair <= secondBoundarySecondPair)){
+            //        count += 1;
+            //        continue;
+            //    }
+            //    
+            ////this checks if the second pair is in the first pair included
+
+            //if((firstBoundaryFirstPair <= firstBoundarySecondPair && firstBoundarySecondPair <= secondBoundaryFirstPair) &&
+            //    (firstBoundaryFirstPair <= secondBoundarySecondPair && secondBoundaryFirstPair >= secondBoundarySecondPair)){
+            //        count += 1;
+            //        continue;
+            //    }
+            
+            //this is for the second Part of Day4
+            if(firstBoundaryFirstPair <= secondBoundarySecondPair && firstBoundarySecondPair <= secondBoundaryFirstPair){ count++; }
 
         }
 
